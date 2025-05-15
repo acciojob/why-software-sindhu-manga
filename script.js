d9bbf0a60163eebca12f04920ab1180850191f8e
sindhu-manga <sindhumanga2330@gmail.com>
2025-05-15 11:50:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
 //your code here
-const button = document.getElementById('motivateBtn');
-button.addEventListener('click', function () {
-    alert("Keep going! You’re on your way to becoming a great developer!");
-});
